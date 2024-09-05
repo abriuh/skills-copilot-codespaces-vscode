64146457d70e6445495e8f8c8e78cc3ba9d9f6f7
Andres Contreras Bernal <35698649+abriuh@users.noreply.github.com>
2024-09-05 01:35:37 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember(){
+        document.getElementById('member').style.display = 'none';
+    document.getElementById('skills').style.display = 'block';
+    document.getElementById('project').style.display = 'none';
+    document.getElementById('contact').style.display = 'none';
+}
